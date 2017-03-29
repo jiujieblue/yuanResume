37f9f3dc92799ffbe85ef575fe00b69947138773
yuan bi zhi <314763667@qq.com>
2017-03-30 00:54:06 +0800

    ybz_

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -47,7 +47,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
         loader: 'url-loader',
         query: {
           limit: 10000,
