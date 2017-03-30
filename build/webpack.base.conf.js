41b9bde0c74357659990b58228f3bd5aead0760f
yuan bi zhi <314763667@qq.com>
2017-03-30 17:13:53 +0800

    ybz

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -53,6 +53,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      { 
+        test: /\.less$/,
+        loaders: ['style', 'css', 'resolve-url', 'less']
       }
     ]
   },
